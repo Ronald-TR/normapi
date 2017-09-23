28435ae7ab0b5cc2d24f4e205072541390575e00
Ronald <ronald-farias@outlook.com>
2017-09-23 19:34:35 -0300

    Update uvgen.py

--- normapi/nucleo/management/commands/uvgen.py
+++ normapi/nucleo/management/commands/uvgen.py
@@ -4,7 +4,66 @@ from importlib import import_module
 import os
 import sys
 
+urlpatterns = """from django.conf.urls import url
+from django.contrib import admin
+from {appname}.views import *
 
+urlpatterns = [
+{urls}
+]
+
+"""
+
+rawurls = """
+    url(r'^{urlname}/$', view{urlname}, name='{urlname}'),
+        """
+
+reservednames = [
+    '__builtins__',
+    '__cached__',
+    '__doc__',
+    '__file__',
+    '__loader__',
+    '__name__',
+    '__package__',
+    '__spec__',
+    'unicode_literals',
+    'models',
+]
+
+views = """from {appname}.models import *
+from django.forms import model_to_dict
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+import datetime
+import json
+
+
+def SerializeModel(model):
+    mdict = model_to_dict(model)
+    for k, v in mdict.items():
+        if type(v) is datetime.date:
+            mdict[k] = v.strftime('%d/%m/%Y')
+        if type(v) is datetime.datetime:
+            mdict[k] = v.isoformat()
+    return json.dumps(mdict)
+
+{indview}
+
+
+"""
+
+rawview = """
+
+@csrf_exempt
+def view{classname}(requests):
+    if requests.method == 'POST':
+        modelo = {classname}.objects.filter(**json.loads(requests.body))
+        jsonModelo = [SerializeModel(m) for m in modelo]
+        return HttpResponse(jsonModelo, content_type='application/json')
+    if requests.method == 'GET':
+        return HttpResponse('erro, verbo indisponivel', 404)
+"""
 
 class Command(BaseCommand):
     help = 'Script para migracao dos modelos da base, traçando rotas de acesso a cada uma delas'
@@ -29,67 +88,6 @@ class Command(BaseCommand):
         with open(dirApp + '\\models.py', 'w') as model:
             sys.stdout = model
             management.call_command('inspectdb', shell=True)
-
-        urlpatterns = """from django.conf.urls import url
-from django.contrib import admin
-from {appname}.views import *
-
-urlpatterns = [
-{urls}
-]
-
-"""
-
-        rawurls = """
-    url(r'^{urlname}/$', view{urlname}, name='{urlname}'),
-        """
-
-        reservednames = [
-            '__builtins__',
-            '__cached__',
-            '__doc__',
-            '__file__',
-            '__loader__',
-            '__name__',
-            '__package__',
-            '__spec__',
-            'unicode_literals',
-            'models',
-        ]
-
-        views = """from {appname}.models import *
-from django.forms import model_to_dict
-from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
-import datetime
-import json
-        
-        
-def SerializeModel(model):
-    mdict = model_to_dict(model)
-    for k, v in mdict.items():
-        if type(v) is datetime.date:
-            mdict[k] = v.strftime('%d/%m/%Y')
-        if type(v) is datetime.datetime:
-            mdict[k] = v.isoformat()
-    return json.dumps(mdict)
-
-{indview}
-
-
-"""
-
-        rawview = """
-
-@csrf_exempt
-def view{classname}(requests):
-    if requests.method == 'POST':
-        modelo = {classname}.objects.filter(**json.loads(requests.body))
-        jsonModelo = [SerializeModel(m) for m in modelo]
-        return HttpResponse(jsonModelo, content_type='application/json')
-    if requests.method == 'GET':
-        return HttpResponse('erro, verbo indisponivel', 404)
-"""
 
         djangoapp = import_module(appname + '.models')
         urls = ''
