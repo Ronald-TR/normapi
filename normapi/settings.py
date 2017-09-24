7bf983f5b7b7e89fc41a80bad8e35e5871b87acf
Ronald <ronald-farias@outlook.com>
2017-09-24 12:55:37 -0300

    Update settings.py
    fixing identation

--- normapi/settings.py
+++ normapi/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'normapi.nucleo',
-    'normapi.clientes',
+    'normapi.nucleo',
 ]
 
 MIDDLEWARE = [
