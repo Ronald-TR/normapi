6a2f0983bba3487edd280636a2e61db228266902
Ronald <ronald-farias@outlook.com>
2017-09-23 21:26:56 -0300

    adicionando factory e eliminando tabelas padroes do django-admin

--- normapi/nucleo/management/commands/uvgen.py
+++ normapi/nucleo/management/commands/uvgen.py
@@ -4,10 +4,95 @@ from importlib import import_module
 import os
 import sys
 
+urlpatterns = """from django.conf.urls import url
+from django.contrib import admin
+from {appname}.views import *
 
+urlpatterns = [
+{urls}
+]
+
+"""
+
+rawurls = """
+    url(r'^{urlname}/$', view{urlname}, name='{urlname}'),
+        """
+
+reservednames = [
+    '__builtins__',
+    '__cached__',
+    '__doc__',
+    '__file__',
+    '__loader__',
+    '__name__',
+    '__package__',
+    '__spec__',
+    'unicode_literals',
+    'models',
+    'AuthGroup',
+    'AuthGroupPermissions',
+    'AuthPermission',
+    'AuthUser',
+    'AuthUserGroups',
+    'AuthUserUserPermissions',
+    'DjangoAdminLog',
+    'DjangoContentType',
+    'DjangoMigrations',
+    'DjangoSession',
+]
+
+views = """from django.forms import model_to_dict
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from django.apps import apps
+import datetime
+import json
+
+
+def SerializeModel(model):
+    mdict = model_to_dict(model)
+    for k, v in mdict.items():
+        if type(v) is datetime.date:
+            mdict[k] = v.strftime('%d/%m/%Y')
+        if type(v) is datetime.datetime:
+            mdict[k] = v.isoformat()
+    return json.dumps(mdict)
+
+
+def Factory(modelname, jsonparams):
+    params = json.loads(jsonparams)
+    orderby = ''
+    try:
+        orderby = str(params.pop('order_by'))
+    except:
+        pass
+    if orderby == '':
+        objeto = apps.get_model('{app}', modelname).objects.filter(**params)
+    else:
+        objeto = apps.get_model('{app}', modelname).objects.filter(**params).order_by('-' + orderby)
+    return objeto
+
+{indview}
+
+
+"""
+
+rawview = """
+
+@csrf_exempt
+def view{classname}(requests):
+    if requests.method == 'POST':
+        
+        modelo = Factory('{classname}', requests.body)
+        jsonModel = [SerializeModel(m) for m in modelo]
+        
+        return HttpResponse(jsonModel, content_type='application/json')
+    if requests.method == 'GET':
+        return HttpResponse('erro, verbo indisponivel', 404)
+"""
 
 class Command(BaseCommand):
-    help = 'Script para migracao dos modelos da base, traçando rotas de acesso a cada uma delas'
+    help = 'Script para migracao dos modelos da base, traçando rotas de acesso a cada uma delas, ignorando as tabelas padrões do django-admin'
 
     #def add_arguments(self, parser):
     #   parser.add_argument('appname', type=str)
@@ -30,66 +115,6 @@ class Command(BaseCommand):
             sys.stdout = model
             management.call_command('inspectdb', shell=True)
 
-        urlpatterns = """from django.conf.urls import url
-from django.contrib import admin
-from {appname}.views import *
-
-urlpatterns = [
-{urls}
-]
-
-"""
-
-        rawurls = """
-            url(r'^{urlname}/$', view{urlname}, name='{urlname}'),
-        """
-
-        reservednames = [
-            '__builtins__',
-            '__cached__',
-            '__doc__',
-            '__file__',
-            '__loader__',
-            '__name__',
-            '__package__',
-            '__spec__',
-            'unicode_literals',
-            'models',
-        ]
-
-        views = """from {appname}.models import *
-from django.forms import model_to_dict
-from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
-import datetime
-import json
-        
-        
-def SerializeModel(model):
-    mdict = model_to_dict(model)
-    for k, v in mdict.items():
-        if type(v) is datetime.date:
-            mdict[k] = v.strftime('%d/%m/%Y')
-        if type(v) is datetime.datetime:
-            mdict[k] = v.isoformat()
-    return json.dumps(mdict)
-
-{indview}
-
-
-"""
-
-        rawview = """
-
-@csrf_exempt
-def view{classname}(requests):
-    if requests.method == 'POST':
-        modelo = {classname}.objects.filter(**json.loads(requests.body))
-            return HttpResponse(SerializeModel(modelo), content_type='application/json')
-        if requests.method == 'GET':
-    return HttpResponse('erro, verbo indisponivel', 404)
-        """
-
         djangoapp = import_module(appname + '.models')
         urls = ''
         indview = ''
@@ -104,5 +129,5 @@ def view{classname}(requests):
             p.write(urlpatterns.format(**{'urls': urls, 'appname': appname}))
 
         with open(dirApp + '\\views.py', 'w') as p:
-            p.write(views.format(**{'indview': indview, 'appname': appname}))
+            p.write(views.format(**{'indview': indview, 'appname': appname, 'app': appname.split('.')[-1]}))
 
